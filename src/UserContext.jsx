2123416db0de970862e266c11e50bae3fb498c5d
Mickael.C <mickael.correia@lagrandeclasse.fr>
2023-03-14 01:02:12 +0100

    minor modif

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -12,7 +12,7 @@ export function UserContextProvider({ children }) {
     // On fait un useEffect pour garder le context lors d'un reload
     useEffect(() => {
         if (!user) {
-            axios.get('https://concertplace-backend.onrender.com/profile').then(({ data }) => {
+            axios.get('/profile').then(({ data }) => {
                 setUser(data);
                 setReady(true);
             });
